31635042650ca5907651f49d5df9e4ef9151e45a
wudibin <781721794@qq.com>
2020-09-21 23:44:29 +0800

    [js-exercise] 4-class

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,14 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    const classString = this.klass == null ? 'No Class.' : `Class ${this.klass}.`;
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach ${classString}`;
+  }
+}
