0d122af3926118b2ae300173a09ea00ed389291a
wudibin <781721794@qq.com>
2020-09-21 23:43:32 +0800

    [js-exercise] 2-array

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,13 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(i => i % 3 == 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return collection.filter((e, i) => {
+    return collection.indexOf(e) === i;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
