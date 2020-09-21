ea60546b70d1ddc9d4043e60340d9e3faf1c25f3
wudibin <781721794@qq.com>
2020-09-21 23:45:02 +0800

    [js-exercise] 5-async

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -1,18 +1,29 @@
 function fetchData(url, successCallback, errorCallback) {
-  const xhr = new XMLHttpRequest();
-  // <-- start
-  // TODO 21: 通过XMLHttpRequest实现异步请求
-
-  // end -->
+    const xhr = new XMLHttpRequest();
+    // <-- start
+    // TODO 21: 通过XMLHttpRequest实现异步请求
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) {
+            return;
+        }
+        if (xhr.status === 200) {
+            successCallback(xhr.responseText);
+        } else {
+            errorCallback(xhr.statusText);
+        }
+    };
+    xhr.send();
+    // end -->
 }
 
 const URL = 'http://localhost:3000/api';
 fetchData(
-  URL,
-  result => {
-    document.writeln(JSON.parse(result).name);
-  },
-  error => {
-    console.error(error);
-  }
+    URL,
+    result => {
+        document.writeln(JSON.parse(result).name);
+    },
+    error => {
+        console.error(error);
+    }
 );
--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -1,18 +1,29 @@
 function fetchData(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    // <-- start
-    // TODO 22: 通过Promise实现异步请求
-
-    // end -->
-  });
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        // <-- start
+        // TODO 22: 通过Promise实现异步请求
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (xhr.status === 200) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        xhr.send();
+        // end -->
+    });
 }
 
 const URL = 'http://localhost:3000/api';
 fetchData(URL)
-  .then(result => {
-    document.writeln(JSON.parse(result).name);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    .then(result => {
+        document.writeln(JSON.parse(result).name);
+    })
+    .catch(error => {
+        console.error(error);
+    });
